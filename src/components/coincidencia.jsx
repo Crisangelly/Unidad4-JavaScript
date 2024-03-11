42577544de4f06527e7dc6b437d25e143fb765ae
Paola Marcano <marcanosalaspv@uvm.edu.ve>
2024-03-11 17:10:39 -0400

    agregar condicional a los datos

--- src/components/coincidencia.jsx
+++ src/components/coincidencia.jsx
@@ -7,7 +7,7 @@ function Coincidencia ({ avatar_url, name, login, company, public_repos }) {
       <ul>
         <li>Nombre: {name}</li>
         <li>Nombre de usuario: {login}</li>
-        <li>Empresa: {company}</li>
+        {company && <li>Empresa: {company}</li>}
         <li>Repositorios públicos: {public_repos}</li>
       </ul>
     </div>
