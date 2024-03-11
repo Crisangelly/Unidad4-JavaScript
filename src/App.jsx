bf30d08fdfd7d3835244f1dc25689de6dec8b152
Paola Marcano <marcanosalaspv@uvm.edu.ve>
2024-03-11 16:14:27 -0400

    mostrar tres usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,8 @@ import API_GITHUB from './conexion_api.js'
 import Coincidencia from './components/coincidencia.jsx';
 
 function App() {
-  const [buscar0, setBuscar0] = useState("");
   const [buscar, setBuscar] = useState("");
-  const [usuario, setUsuario] = useState([])
+  const [usuarios, setUsuarios] = useState([])
 
   //Buscar usuarios
   const fETCH_USERS = async () => {
@@ -16,40 +15,39 @@ function App() {
       params: {
         per_page: 3, //limitar resultados a tres
         page: 1,
-        q: buscar0
+        q: buscar
       }
     });
     console.debug(RESPONSE.data);
     //console.debug(RESPONSE.data.items)
     let usuarioV = '';
+    let usuarios_3 = [];
     for (const user of RESPONSE.data.items) {
-      console.log(user.login) //mostrar usuarios
+      console.log(user) //mostrar usuarios
       usuarioV = user.login
+      const RESPONSE2 = await API_GITHUB.get(`/users/${usuarioV}`);//Mostrar el último usuario (ARREGLAR para mostrar los tres)
+      usuarios_3.push(RESPONSE2.data)
     }
-    const RESPONSE2 = await API_GITHUB.get(`/users/${usuarioV}`);//Mostrar el último usuario (ARREGLAR para mostrar los tres)
-    return setUsuario(RESPONSE2.data);
-  }
-
-
-  //Consultar datos de usuario
-  const fETCH_DATA = async () => {
-    const RESPONSE = await API_GITHUB.get(`/users/${buscar}`); //Para ver otro usuario se cambia la url
-    return setUsuario(RESPONSE.data);
+    return setUsuarios(usuarios_3);
   }
 
   return (
     <>
-      <input type="text" placeholder="Buscar Users" onChange={(e) => setBuscar0(e.target.value)}></input>
+      <input type="text" placeholder="Buscar Users" onChange={(e) => setBuscar(e.target.value)}></input>
       <button onClick={fETCH_USERS}>Buscar</button>
-      <input type="text" placeholder="Buscar" onChange={(e) => setBuscar(e.target.value)}></input>
-      <button onClick={fETCH_DATA}>Buscar</button>
-      <Coincidencia
-        avatar_url={usuario.avatar_url}
-        name={usuario.name}
-        login={usuario.login}
-        company={usuario.company}
-        public_repos={usuario.public_repos}
-      />
+      {
+        usuarios.map(usuario => (
+          <Coincidencia
+            key={usuario.id}
+            avatar_url={usuario.avatar_url}
+            name={usuario.name}
+            login={usuario.login}
+            company={usuario.company}
+            public_repos={usuario.public_repos}
+          />
+        ))
+      } 
+
     </>
   )
 }
