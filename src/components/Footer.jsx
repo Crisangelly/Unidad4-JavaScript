85d437c911bc0debd723c0eb7a8964f6b6285ebb
Paola Marcano <marcanosalaspv@uvm.edu.ve>
2024-03-14 22:36:17 -0400

    arreglar enlace del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "../styles/footer.css"
 function Footer (){
     return(
         <footer className="footer">
-          <a className="a-footer" href="https://github.com/Crisangelly/Unidad4-JavaScript">Repositorio Github</a>
+          <a className="a-footer" href="https://github.com/Crisangelly/Unidad4-JavaScript" target="_blank">Repositorio Github</a>
           <p className="p-footer" >©copyrigth2024</p>
         </footer>
     );
