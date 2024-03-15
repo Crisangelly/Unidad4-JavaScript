828ae2f9a3a29b60bbaf13888f177f2e49039400
Paola Marcano <marcanosalaspv@uvm.edu.ve>
2024-03-14 21:21:51 -0400

    redireccionar css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import Header from './components/Header.jsx';
 import App from './App.jsx'
 import './index.css'
-import './styles/header.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
