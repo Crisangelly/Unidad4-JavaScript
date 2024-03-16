246c23d00bb588869703eee8d4f6bbb742b850e6
Crisangelly <hernandezfernandezcd@uvm.edu.ve>
2024-03-16 05:14:21 -0400

    mejoras landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,11 +7,14 @@ function LandingPage() {
     <>
       <main className="landingPage-main">
         <div className="info-div">
-          <h1 className="info-h1">¡Bienvenido!</h1>
+          <div className="conteiner-h1">{/*Cris*/}
+          < h1 className="info-h1">¡Bienvenido!</h1>
+          </div>
           <p className="info-p">Busca los usuarios que desees fácil y rápido</p>
           <Link to="/buscador" className="link-landingPage">Comienza Ahora</Link>
         </div>
         <div className="octocat-div">
+          <h1 className="info-h11">¡Bienvenido!</h1>
           <img src={OctocatLogo} alt="Octocat" className="octocat-img"></img>
         </div>
       </main>
