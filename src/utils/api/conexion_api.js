85e12ba226cf7dfdc2c50b620ef7b6ddea0d5740
Paola Marcano <marcanosalaspv@uvm.edu.ve>
2024-03-18 13:56:16 -0400

    crear token

--- src/utils/api/conexion_api.js
+++ src/utils/api/conexion_api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
+import dotenv from "dotenv"
 
 const URL_BASE = "https://api.github.com/";
 
 const API_GITHUB = axios.create({
-  baseURL: URL_BASE
+  baseURL: URL_BASE,
+  headers: {'Authorization': dotenv.SECRET}
 });
 
 export default API_GITHUB;
